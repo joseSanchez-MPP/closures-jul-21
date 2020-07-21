5091bc52047fdd338f696fc369a897c211925ca5
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-21 13:29:45 -0500

    Hopefully this is finished. Hopefully

--- Jose_S_ClosureExercise.playground/Contents.swift
+++ Jose_S_ClosureExercise.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-//I think this is what was supposed to be done, though I'm not sure
+//I think this is what was supposed to be done, though I'm not exactly sure.
 
 let multiply = { (n1: Int, n2: Int) -> Void in
     print(n1 * n2)
